581a6e04e3b3179cd2cf06b9db1652dab7a5ba50
Your Name <you@example.com>
2024-12-01 11:03:44 +0200

    newSEO

--- src/screens/FirstScreen.js
+++ src/screens/FirstScreen.js
@@ -1,7 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './FirstScreen.module.css';
 import { ChevronDown } from 'lucide-react';
+import LoadingEffect from '../components/loader/Loader';
+import image1 from "../images/סוזנה פנחסי ימין.png"
+import image2 from "../images/סוזנה פנחסי שמאל.png"
+import image3 from "../images/סוזנה פנחסי מרכז.png"
+
 const FirstScreen = () => {
+  const [imagesLoaded, setImagesLoaded] = useState(false);
+  
+  useEffect(() => {
+    const backgroundImages = [
+   image1,image2,image3
+    ];
+
+    const loadImage = (imageSrc) => {
+      return new Promise((resolve, reject) => {
+        const img = new Image();
+        img.src = imageSrc;
+        img.onload = resolve;
+        img.onerror = reject;
+      });
+    };
+
+    Promise.all(backgroundImages.map(imageSrc => loadImage(imageSrc)))
+      .then(() => {
+        setImagesLoaded(true);
+      })
+      .catch((err) => {
+        console.error("Error loading images", err);
+        // You might want to set imagesLoaded to true even on error
+        // to show the content instead of infinite loading
+        setImagesLoaded(true);
+      });
+  }, []);
+
+  if (!imagesLoaded) {
+    return <LoadingEffect />;
+  }
+
   return (
     <div className={styles.container}>
       <div className={styles.imageContainer}>
@@ -26,9 +63,7 @@ const FirstScreen = () => {
             <ChevronDown className={styles.arrow} size={38} strokeWidth={1.5}/>
           </div>
         </div>
-     
       </div>
-   
     </div>
   );
 };
